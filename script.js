6fedb90bb4fdd2f4eed8c32c76b2da835a00deaa
FenilJethwa <w0805286@myscc.ca>
2022-09-16 18:10:04 -0400

    Update with some comment.
    Update with some comment and I am done with my assignment1.

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 /*
 	WEB 303 Assignment 1 - jQuery
-	{Fenil Jethwa}
+	{Fenil Jethwa} Name
+	{0805286} Student ID
 */
 
 $(document).ready(function(){
+// changeEvent //	
 	$("input").change(function(){
 		let Wage = $("#yearly-salary").val();
 		let Proportion = $("#percent").val();
 		let spentamt = ((Wage*Proportion)/100).toFixed(2);
 
+// Calculation  //
 		$("#amount").text("$" + spentamt);
 	});
 });
